d40182e43ca55ae4aa88de5a34e8b3943c2aee06
Michael George Nelson <nelso430@purdue.edu>
2023-11-22 16:59:29 -0500

    Get Image returns an image that can be loaded into the canvas

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, send_file
 import base64
+import re
+from io import BytesIO
+from PIL import Image
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/')
 def index():
@@ -15,6 +20,18 @@ def submit():
         image_data = image_file.read()
         encoded_image = base64.b64encode(image_data).decode('utf-8')
         return render_template('result.html', text=text_input, image_data=encoded_image)
+    
+@app.route('/getImage', methods=['POST'])
+def getImage():
+    text_input = request.form['prompt']
+    #image_file = request.files['image']
+    data_url = request.values['image']
+    offset = data_url.index(',')
+    img_bytes = base64.b64decode(data_url[offset:])
+    img = BytesIO(img_bytes)
+    # img = Image.open(BytesIO(img_bytes))
+    # img.show()
+    return send_file(img, mimetype='image/png') 
 
 if __name__ == '__main__':
     app.run(debug=True)
